40b557e8b0b9d69a7fcaf964bd3ac7730383cd1a
Jesus163174 <163174@ids.upchiapas.edu.mx>
2019-05-30 15:00:00 -0500

    dashboard

--- app/resources/views/users/index.blade.php
+++ app/resources/views/users/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.admin')
-@section('title','Admin')
+@section('title','Usuarios')
 @section('content')
 
 @stop
--- app/routes/web.php
+++ app/routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','AdminController@index')->middleware('auth');
 
 
 Route::group(['prefix'=>'/admin'],function(){
