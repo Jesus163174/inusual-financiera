8fe0c265be2fc40d205bcf5723a6c143c2474dbb
Jesus163174 <163174@ids.upchiapas.edu.mx>
2019-05-30 13:58:47 -0500

    Controlador usuarios listo

---
+++ app/app/Http/Controllers/UserController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    
+    public function index(){
+        $users = User::employe()->get();
+        return view('users.index',compact('users'));
+    }
+    public function create(){
+        $user = new User();
+        return view('users.create',compact('user'));
+    }
+    public function store(Request $request){
+        try{
+            $user = new User();
+            $validate = $user->validate($request);
+            $user->store($validate);
+            $msj = "Usuario registrado correctamente";
+            return redirect('/admin/usuarios')->with('status_succes',$msj);
+        }catch(Exception $e){
+            return back()->with('status_danget',"Error en el servidor: ".$e->getMessage());
+        }
+    }
+    public function show($id){
+        $user = User::find($id);
+        return view('users.show',compact('user'));
+    }
+    public function edit($id){
+        $user = User::find($id);
+        return view('users.edit',compact('user'));
+    }
+    public function update(Request $request, $id){
+        try{
+            $user = User::find($id);
+            $user->edit($request);
+            $msj = "Usuario actualizado correctamente";
+            return redirect('/admin/usuarios')->with('status_succes',$msj);
+        }catch(Exception $e){
+            return back()->with('status_danget',"Error en el servidor: ".$e->getMessage());
+        }
+    }
+    public function destroy($id){
+        $user = User::find($id);
+        if($user->status == 'eliminado')
+            $user->status = 'activo';
+        else
+            $user->status = 'eliminado';
+        $user->save();
+        return redirect('/admin/usuarios');
+    }
+}
